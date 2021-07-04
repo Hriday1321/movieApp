06fc55858c0db2ac641117b7ce039b3977aa7e31
EC2 Default User <hridaygupta100@hotmail.com>
2021-07-04 10:30:38 +0000

    made sample data not to have to worry about overusing api key

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var request = require("request");
+/*var request = require("request");
 
 request("http://www.omdbapi.com/?apikey=298f6e5e&t=schindler", function (error, response, body) {
 	if(!error && response.statusCode == 200)
@@ -6,4 +6,37 @@ request("http://www.omdbapi.com/?apikey=298f6e5e&t=schindler", function (error, 
        var data = JSON.parse(body);
        console.log(data);
     }
-});
+});*/
+
+var sampleData = { Title: 'Schindler: The Real Story',
+  Year: '1983',
+  Rated: 'N/A',
+  Released: 'N/A',
+  Runtime: 'N/A',
+  Genre: 'Documentary',
+  Director: 'Jon Blair',
+  Writer: 'Jon Blair',
+  Actors: 'Dirk Bogarde, Emilie Schindler, Oskar Schindler',
+  Plot:
+   'The true story of Oskar Schindler\'s exploits in which he saved a thousand Jews from the ovens of the death camps, s told by first hand witnesses. The British Academy Award winning ...',
+  Language: 'English, German, Polish',
+  Country: 'UK',
+  Awards: '1 win.',
+  Poster:
+   'https://m.media-amazon.com/images/M/MV5BMTY4MTY0OTY4NV5BMl5BanBnXkFtZTcwOTgxMjMyMQ@@._V1_SX300.jpg',
+  Ratings: [ { Source: 'Internet Movie Database', Value: '7.4/10' } ],
+  Metascore: 'N/A',
+  imdbRating: '7.4',
+  imdbVotes: '259',
+  imdbID: 'tt0180981',
+  Type: 'movie',
+  DVD: 'N/A',
+  BoxOffice: 'N/A',
+  Production: 'N/A',
+  Website: 'N/A',
+  Response: 'True' 
+};
+
+console.log(sampleData);
+
+//using sample data not to ovruse api key given to me
