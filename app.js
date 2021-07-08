2eaa27c754f1421c4bec66551b8e714dfa178a6d
EC2 Default User <hridaygupta100@hotmail.com>
2021-07-08 10:39:47 +0000

    completed functionality

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var request = require("request");
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
@@ -16,6 +19,28 @@ request("http://www.omdbapi.com/?apikey=298f6e5e&s=schindler", function (error, 
 
 app.get("/", function(req, res){
   res.render("home.ejs", {data: data});
+})
+
+app.get("/title", function(req,res){
+    request("http://www.omdbapi.com/?apikey=298f6e5e&t=" + req.query.title, function (error, response, body) {
+	    if(!error && response.statusCode == 200)
+        {
+           var tdata = JSON.parse(body);
+           data = tdata;
+        }
+        res.render("title.ejs", {data: data});
+    });   
+})
+
+app.get("/search", function(req,res){
+    request("http://www.omdbapi.com/?apikey=298f6e5e&s=" + req.query.search, function (error, response, body) {
+	    if(!error && response.statusCode == 200)
+        {
+           var tdata = JSON.parse(body);
+           data = tdata;
+        }
+        res.render("search.ejs", {data: data});
+    });
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
